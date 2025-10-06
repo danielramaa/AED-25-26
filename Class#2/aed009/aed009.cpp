4fb859c6d7b746407a3dd16357006a882142ead0
danielramaa <daniel.morais.rama@gmail.com>
2025-10-06 22:19:12 +0100

    aed009

---
+++ Class#2/aed009/aed009.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n;
+    std::cin >> n;
+
+    std::vector<int> sequence(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> sequence[i];
+    }
+
+    int max_so_far = sequence[0];
+    int max_ending_here = sequence[0];
+
+    for (int i = 1; i < n; ++i) {
+        int current_number = sequence[i];
+        max_ending_here = std::max(current_number, max_ending_here + current_number);
+        max_so_far = std::max(max_so_far, max_ending_here);
+    }
+
+    std::cout << max_so_far << std::endl;
+
+    return 0;
+}
